b602f9770eb577709d0984c3e91a8c2ef9c0863d
majo-z <g00340481@gmit.ie>
2019-02-05 11:59:02 +0000

    defined getters & setters & object init in hangperson_game.rb, enabled first test

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -5,13 +5,17 @@ class HangpersonGame
 
   # Get a word from remote "random word" service
 
-  # def initialize()
-  # end
-  
+  # define getters & setters
+  attr_accessor :word, :guesses, :wrong_guesses
+
+  #initialize object
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
   end
 
+  
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
