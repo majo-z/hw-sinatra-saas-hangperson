a4a3aa6012501f6acafd5f6ef640b0e8ae6cca20
majo-z <g00340481@gmit.ie>
2019-02-08 18:05:33 +0000

    added methods to hangperson_game & enabled more tests

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -15,6 +15,16 @@ class HangpersonGame
     @wrong_guesses = ""
   end
 
+  # instance methods of the game class
+
+  def guess
+  end
+
+  def check_win_or_lose
+  end
+
+  def word_with_guesses
+  end
   
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
