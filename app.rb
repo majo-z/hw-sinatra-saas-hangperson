b5604002b0cb3be79ca701f525918514add987f3
majo-z <g00340481@gmit.ie>
2019-02-19 04:09:00 +0000

    added conditions to controller to redirect after game finished

--- app.rb
+++ app.rb
@@ -62,8 +62,13 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+      redirect '/lose'
+    else
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
